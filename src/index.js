4679e67b4e055034dc7925a43ba7a00d4a3d7230
ebagabee <gh.alvesantos@gmail.com>
2025-12-23 20:07:39 -0300

    feat: create functions to clean word, break paragraph and verify duplicates.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ const fs = require("fs");
 const filepath = process.argv;
 const link = filepath[2];
 
+const checkDuplicateWords = (txt) => {
+  const words = txt.split(" ");
+  const foundWords = {};
+
+  words.forEach((word) => {
+    if (word.length >= 3) {
+      const wordCleanup = cleanWord(word);
+      foundWords[wordCleanup] = (foundWords[wordCleanup] || 0) + 1;
+    }
+  });
+
+  return foundWords;
+};
+
+const paragraphBreak = (text) => {
+  const paragraphs = text.toLowerCase().split("\n");
+
+  const count = paragraphs.flatMap((paragraph) => {
+    if (!paragraph) return [];
+    return checkDuplicateWords(paragraph);
+  });
+
+  return count;
+};
+
+const cleanWord = (word) => {
+  return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+};
+
 fs.readFile(link, "utf-8", (err, txt) => {
-  console.log(txt);
+  console.log(paragraphBreak(txt));
 });
