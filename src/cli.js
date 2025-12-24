100332a29df6dbd646340f110af82491840230da
ebagabee <gh.alvesantos@gmail.com>
2025-12-24 15:06:58 -0300

    feat: create a new file with filesystem

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,25 @@ import { countWords } from "./index.js";
 
 const filepath = process.argv;
 const link = filepath[2];
+const path = filepath[3];
 
 fs.readFile(link, "utf-8", (err, txt) => {
   try {
     if (err) throw err;
-
-    console.log(countWords(txt));
+    const result = countWords(txt);
+    createAndSaveFile(result, path);
   } catch (err) {
     handleErrors(err);
   }
 });
+
+const createAndSaveFile = async (listWords, path) => {
+  const newFile = `${path}/result.txt`;
+  const content = JSON.stringify(listWords);
+  try {
+    await fs.promises.writeFile(newFile, content);
+    console.log("File created!");
+  } catch (error) {
+    throw error;
+  }
+};
