c34c8e00028292c446c4b3ec508f83a188abe578
ebagabee <gh.alvesantos@gmail.com>
2025-12-24 14:19:02 -0300

    feat: create directory and function to handle errors in the code.

---
+++ src/errors/errorFunction.js
@@ -0,0 +1,9 @@
+const handleErrors = (err) => {
+  if (err.code === "ENOENT") {
+    throw new Error("Not found file");
+  }
+
+  throw new Error("Error in the server");
+};
+
+module.exports = handleErrors;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 const fs = require("fs");
+const handleErrors = require("./errors/errorFunction");
 
 const filepath = process.argv;
 const link = filepath[2];
+
+fs.readFile(link, "utf-8", (err, txt) => {
+  try {
+    if (err) throw err;
+
+    console.log(countWords(txt));
+  } catch (err) {
+    handleErrors(err);
+  }
+});
+
+const countWords = (text) => {
+  const paragraphs = extractParagraphs(text);
+
+  const count = paragraphs.flatMap((paragraph) => {
+    if (!paragraph) return [];
+    return checkDuplicateWords(paragraph);
+  });
+
+  return count;
+};
+
+const extractParagraphs = (text) => {
+  return text.toLowerCase().split("\n");
+};
+
+const cleanWord = (word) => {
+  return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+};
 
 const checkDuplicateWords = (txt) => {
   const words = txt.split(" ");
@@ -16,22 +46,3 @@ const checkDuplicateWords = (txt) => {
 
   return foundWords;
 };
-
-const paragraphBreak = (text) => {
-  const paragraphs = text.toLowerCase().split("\n");
-
-  const count = paragraphs.flatMap((paragraph) => {
-    if (!paragraph) return [];
-    return checkDuplicateWords(paragraph);
-  });
-
-  return count;
-};
-
-const cleanWord = (word) => {
-  return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-};
-
-fs.readFile(link, "utf-8", (err, txt) => {
-  console.log(paragraphBreak(txt));
-});
