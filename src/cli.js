ab8a5bfa59e6cf50c995362a8f82edbd85658154
ebagabee <gh.alvesantos@gmail.com>
2025-12-25 14:25:05 -0300

    feat: add a commander lib to help the user to doc

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,23 @@
 import fs from "fs";
+import path from "path";
 import { handleErrors } from "./errors/errorFunction.js";
 import { countWords } from "./index.js";
 import { mountOutputFile } from "./helpers.js";
 import { Command } from "commander";
 
 const program = new Command();
+
+const proccessFile = (text, destination) => {
+  fs.readFile(text, "utf-8", (err, txt) => {
+    try {
+      if (err) throw err;
+      const result = countWords(txt);
+      createAndSaveFile(result, destination);
+    } catch (err) {
+      handleErrors(err);
+    }
+  });
+};
 
 program
   .version("0.0.1")
@@ -21,21 +34,19 @@ program
       program.help();
       return;
     }
+
+    const textPath = path.resolve(text);
+    const destiantionPath = path.resolve(destination);
+
+    try {
+      proccessFile(textPath, destiantionPath);
+      console.log("text proccessed successfully!");
+    } catch (error) {
+      console.log("Proccess error file", error);
+    }
   });
 
-const filepath = process.argv;
-const link = filepath[2];
-const path = filepath[3];
-
-fs.readFile(link, "utf-8", (err, txt) => {
-  try {
-    if (err) throw err;
-    const result = countWords(txt);
-    createAndSaveFile(result, path);
-  } catch (err) {
-    handleErrors(err);
-  }
-});
+program.parse();
 
 const createAndSaveFile = (listWords, path) => {
   const newFile = `${path}/result.txt`;
